4733c740deb6b34c2d5dd212160fea434701ff43
Mike Appleby <mike@peacecorps.org.cv>
2014-06-09 19:42:41 -0500

    Add ConsCell class.

---
+++ src/cons.h
@@ -0,0 +1,20 @@
+#ifndef MCLISP_CONS_H_
+#define MCLISP_CONS_H_
+
+namespace mclisp
+{
+
+class ConsCell
+{
+public:
+    ConsCell(): car_(nullptr), cdr_(nullptr) {}
+    ConsCell(ConsCell* car, ConsCell* cdr): car_(car), cdr_(cdr) {}
+
+private:
+    ConsCell* car_;
+    ConsCell* cdr_;
+};
+
+} // namespace mclisp
+
+#endif // MCLISP_CONS_H_
