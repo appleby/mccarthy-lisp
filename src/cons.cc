39d3fe869f690b0f3880da88b3fcdb94200b164b
Mike Appleby <mike@peacecorps.org.cv>
2014-06-26 21:54:38 -0500

    Implement MakeAssociationList and MakePnameList.

--- src/cons.cc
+++ src/cons.cc
@@ -1,21 +1,69 @@
+#include <cassert>
+#include <cstddef>
 #include <cstring>
 
 #include "alloc.h"
 #include "cons.h"
 
+namespace
+{
+using namespace mclisp;
+
+const std::string kPname = "PNAME";
+
+void SetCons(ConsCell** c, const char* s)
+{
+  strncpy(reinterpret_cast<char *>(c), s, sizeof c);
+}
+
+std::string Slurp(ConsCell** c, const std::string& s)
+{
+  SetCons(c, s.c_str());
+  return s.length() > sizeof c ? s.substr(sizeof c, std::string::npos) : "";
+}
+
+ConsCell* MakePnameList(const std::string& name)
+{
+  ConsCell *head, *prev, *curr, *data;
+  std::string rest(name);
+
+  for (head = prev = curr = Alloc::Allocate();
+       rest.length() > 0;
+       curr = Alloc::Allocate())
+  {
+    data = Alloc::Allocate();
+    rest = Slurp(&data->car_, rest);
+    rest = Slurp(&data->cdr_, rest);
+
+    prev->cdr_ = curr;
+    curr->car_ = data;
+    prev = curr;
+  }
+
+  curr->cdr_ = const_cast<ConsCell*>(kNil);
+  return head;
+}
+
+ConsCell* MakeAssociationList(const std::string& name)
+{
+  ConsCell* pname = Alloc::Allocate();
+  ConsCell* link = Alloc::Allocate();
+
+  SetCons(&pname->car_, kPname.c_str());
+  pname->cdr_ = link;
+
+  link->car_ = MakePnameList(name);
+  link->cdr_ = const_cast<ConsCell*>(kNil);
+
+  return pname;
+}
+
+} // namespace
+
 namespace mclisp
 {
 const ConsCell* kT = nullptr;
 const ConsCell* kNil = nullptr;
-
-//static constexpr char* kPname = "PNAME";
-
-static ConsCell* MakeAssociationList(const std::string& name)
-{
-  // TODO
-  ConsCell* c = Alloc::Allocate();
-  return c;
-}
 
 const ConsCell* MakeCons(const ConsCell* car, const ConsCell* cdr)
 {
@@ -27,6 +75,7 @@ const ConsCell* MakeCons(const ConsCell* car, const ConsCell* cdr)
 
 const ConsCell* MakeSymbol(const std::string& name)
 {
+  assert(name.length());
   ConsCell* c = Alloc::Allocate();
   c->car_ = const_cast<ConsCell*>(Alloc::AtomMagic());
   c->cdr_ = MakeAssociationList(name);
