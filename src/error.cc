57207f07ea520896fe589d1de5403e738a698ff2
Mike Appleby <mike@peacecorps.org.cv>
2014-07-10 21:21:44 -0500

    Use strncpy to build the error message for exceptions.

--- src/error.cc
+++ src/error.cc
@@ -1,36 +1,47 @@
+#include <algorithm>
 #include <cstring>
 #include "error.h"
 
+#define MAYBE_COPY(buf, space, str) \
+  if (space) { \
+    const char *s = (str); \
+    size_t n = std::min(strlen(s), space); \
+    strncpy(buf, s, n); \
+    buf += n; \
+    space -= n; \
+    *buf = '\0'; \
+  }
+
 namespace mclisp
 {
-
 const char* Error::what() const noexcept
 {
-  static char buf[256];
-  // TODO strncpy / strncat
-  strcpy(buf, prefix());
-  strcat(buf, " ");
-  strcat(buf, std::logic_error::what());
-  return buf;
+  static char buffer[256];
+  char *buf = buffer;
+  size_t space = sizeof buffer - 1;
+
+  MAYBE_COPY(buf, space, prefix());
+  MAYBE_COPY(buf, space, " ");
+  MAYBE_COPY(buf, space, std::logic_error::what());
+  return buffer;
 }
 
 const char* TypeError::what() const noexcept
 {
-  // TODO strncpy / strncat
   // TODO don't call function that might throw.
-  static char buf[256];
+  static char buffer[256];
+  char *buf = buffer;
+  size_t space = sizeof buffer - 1;
+
   std::string objstr = ToString(object_);
 
-  strcpy(buf, prefix());
-  strcat(buf, " '");
-  strncat(buf, objstr.c_str(), 32);
-
-  if (objstr.length() > 32)
-    strcat(buf, "...");
-
-  strcat(buf, "' does not satisfy ");
-  strcat(buf, should_satisfy_.c_str());
-  strcat(buf, ".");
-  return buf;
+  MAYBE_COPY(buf, space, prefix());
+  MAYBE_COPY(buf, space, " '");
+  // TODO truncate objstr?
+  MAYBE_COPY(buf, space, objstr.c_str());
+  MAYBE_COPY(buf, space, "' does not satisfy ");
+  MAYBE_COPY(buf, space, should_satisfy_.c_str());
+  MAYBE_COPY(buf, space, ".");
+  return buffer;
 }
 } // mclisp
