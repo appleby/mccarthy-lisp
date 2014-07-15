cf8b4a7849d7bb94206ba78681a7252b1cc6ccc6
Mike Appleby <mike@peacecorps.org.cv>
2014-07-15 18:09:20 -0500

    Add comment explaining defun brokeness.

--- src/eval.cc
+++ src/eval.cc
@@ -65,9 +65,36 @@ ConsCell *Eval(const ConsCell *exp, ConsCell *env /* env::g_user_env */)
 
     if (EQ(Car(exp), DEFUN))
     {
-      // TODO fix defun.
+      // TODO fix defun (maybe).
       //
-      // This won't work if the defun isn't a toplevel form.
+      // This won't work as expected if the defun isn't a toplevel form. If a
+      // non-toplevel defun is encountered, the binding it creates won't be
+      // available until the next toplevel form executes, since we extend the
+      // global user env, rather the current dynamic env. For example, the
+      // following will cause an UnboundSymbol exception due to foo not being
+      // bound in the body of the lambda:
+      //
+      // ((lambda (ignore) (foo))
+      //  (defun foo () 'foo))
+      //
+      // Not that any sane person would write code like that... The defun
+      // brokeness would be a bigger deal if mclisp supported some kind of
+      // progn, implicit or otherwise, in which case one might reasonably expect
+      // to be able to write internal defuns, like so:
+      //
+      // (defun foo (x)
+      //    (defun helper (a) ...)
+      //    (helper x))
+      //
+      // Even this is not idiomatic lisp, and it's unlikely you would want an
+      // internal defun to both create a global binding *and* make the binding
+      // immediately available in current env.  More likely, you want only the
+      // latter, which can be accomplished in mclisp with a label expression.
+      //
+      // All the above whining aside, it wouldn't be hard to fix this by either
+      // 1) restricting defuns to toplevel forms only, or 2) sticking the
+      // binding on the symbol's plist, and falling back to plist lookup if
+      // lookup in the dynamic env fails.
       ConsCell *fname = Cadr(exp);
       ConsCell *formals = Caddr(exp);
       ConsCell *body = Cdddr(exp);
