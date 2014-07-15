87c883aa6179554787a00ba9cebb625a71e22d98
Mike Appleby <mike@peacecorps.org.cv>
2014-07-14 20:11:52 -0500

    Update the REPL to recognize a (quit) form.

--- src/main.cc
+++ src/main.cc
@@ -36,7 +36,7 @@ int Repl::loop()
       mclisp::ConsCell *exp = reader_.Read();
       oss << *exp;
 
-      if (oss.str() == "QUIT" || oss.str() == "(QUIT . NIL)")
+      if (oss.str() == "QUIT" || oss.str() == "(QUIT)")
         // TODO implement real (quit) function.
         break;
 
