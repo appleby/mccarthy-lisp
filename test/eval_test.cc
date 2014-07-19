2897f35f3dc7347ed1d2b4f836b85ca02eb6f561
Mike Appleby <mike@app.leby.org>
2014-07-19 14:48:04 -0500

    Use value parameterized tests to run the Eval tests.

--- test/eval_test.cc
+++ test/eval_test.cc
@@ -1,16 +1,97 @@
 #include "eval.h"
-#include "gtest/gtest.h"
-#include "cons.h"
-#include "env.h"
 #include "reader.h"
 
-#include <iostream>
+#include <map>
+#include <tuple>
+#include <vector>
 
-// TODO Reduce boilerplate in these tests.
+#include "gtest/gtest.h"
 
 namespace
 {
 using namespace mclisp;
+
+class EvalTest :
+  public ::testing::TestWithParam< std::tuple<const char*, const char*> >
+{
+  private:
+    static std::map<std::string, ConsCell*> objects_;
+    static bool objects_initialized_;
+
+  protected:
+    EvalTest()
+    {
+      if (!objects_initialized_)
+      {
+        ConsCell *foo = reader::Intern("FOO");
+        ConsCell *bar = reader::Intern("BAR");
+
+        objects_.emplace("t", kT);
+        objects_.emplace("nil", kNil);
+        objects_.emplace("foo", foo);
+        objects_.emplace("bar", bar);
+        objects_.emplace("foobar", Cons(foo, bar));
+        objects_.emplace("barfoo", Cons(bar, foo));
+        objects_.emplace("foo_list", List(foo));
+        objects_.emplace("bar_list", List(bar));
+        objects_.emplace("foobar_list", List(foo, bar));
+        objects_initialized_ = true;
+      }
+
+      expr_ = std::get<0>(GetParam());
+      expect_ = objects_[std::string(std::get<1>(GetParam()))];
+    }
+
+    std::string expr_;
+    ConsCell* expect_;
+};
+
+std::map<std::string, ConsCell*> EvalTest::objects_;
+bool EvalTest::objects_initialized_ = false;
+
+#define TUP std::make_tuple
+INSTANTIATE_TEST_CASE_P(
+ Eval, EvalTest,
+ ::testing::Values(
+  TUP("t", "t"),
+  TUP("nil", "nil"),
+
+  TUP("(quote, foo)", "foo"),
+  TUP("(quote, (foo . bar))", "foobar"),
+  TUP("(quote, (foo, bar))", "foobar_list"),
+
+  TUP("(atom, t)", "t"),
+  TUP("(atom, nil)", "t"),
+  TUP("(atom, 'foo)", "t"),
+  TUP("(atom, 'foo bar)", "t"),
+  TUP("(atom, 'apple pie number 3)", "t"),
+  TUP("(atom, '(foo))", "nil"),
+  TUP("(atom, '(foo, bar))", "nil"),
+  TUP("(atom, '(foo . bar))", "nil"),
+
+  TUP("(eq, t, t)", "t"),
+  TUP("(eq, nil, nil)", "t"),
+  TUP("(eq, 'foo, 'foo)", "t"),
+  TUP("(eq, t, nil)", "nil"),
+  TUP("(eq, 'foo, 'bar)", "nil"),
+  TUP("(eq, 'foo, '(foo))", "nil"),
+  TUP("(eq, '(foo), '(foo))", "nil"),
+
+  TUP("(cons, 'foo, 'bar)", "foobar"),
+  TUP("(cons, 'foo, '(bar))", "foobar_list"),
+
+  TUP("(car, '(foo, bar))", "foo"),
+  TUP("(car, '((foo, bar), foo))", "foobar_list"),
+
+  TUP("(cdr, '(foo, bar))", "bar_list"),
+  TUP("(cdr, '((foo, bar), foo))", "foo_list"),
+
+  TUP("(cond)", "nil"),
+  TUP("(cond, (nil, t))", "nil"),
+  TUP("(cond, (nil, unbound))", "nil"),
+  TUP("(cond, (nil, t), (t, 'foo))", "foo"),
+  TUP("(cond, (nil, t), ((atom, 'foo), 'foo))", "foo")));
+#undef TUP
 
 ConsCell* EvalString(const std::string& expr)
 {
@@ -18,157 +99,18 @@ ConsCell* EvalString(const std::string& expr)
   return Eval(reader.Read());
 }
 
-TEST(EvalTest, T)
+TEST_P(EvalTest, TestExpr)
 {
-  Reader reader("t");
-  ConsCell* exp = reader.Read();
-  EXPECT_EQ(*kT, *Eval(exp));
-}
-
-TEST(EvalTest, NIL)
-{
-  Reader reader("nil");
-  ConsCell* exp = reader.Read();
-  EXPECT_EQ(*kNil, *Eval(exp));
-}
-
-TEST(EvalTest, UnboundSymbol)
-{
-  Reader reader("unbound");
-  ConsCell* exp = reader.Read();
-  EXPECT_THROW(Eval(exp), env::UnboundSymbolError);
-}
-
-TEST(EvalTest, QuotedSymbol)
-{
-  Reader reader("(quote, foo)");
-  ConsCell* exp = reader.Read();
-  ConsCell* foo = reader::Intern("foo");
-  EXPECT_EQ(*foo, *Eval(exp));
-}
-
-TEST(EvalTest, QuotedCons)
-{
-  Reader reader("(quote, (foo . bar))");
-  ConsCell* exp = reader.Read();
-  ConsCell* foo = reader::Intern("foo");
-  ConsCell* bar = reader::Intern("bar");
-  ConsCell* C1 = Cons(foo, bar);
-  EXPECT_EQ(*C1, *Eval(exp));
-}
-
-TEST(EvalTest, QuotedList)
-{
-  Reader reader("(quote, (foo, bar))");
-  ConsCell* exp = reader.Read();
-  ConsCell* foo = reader::Intern("foo");
-  ConsCell* bar = reader::Intern("bar");
-  ConsCell* C1 = Cons(bar, kNil);
-  ConsCell* C2 = Cons(foo, C1);
-  EXPECT_EQ(*C2, *Eval(exp));
-}
-
-class EvalAtomTest :
-  public ::testing::TestWithParam< std::tuple<std::string, ConsCell *> >
-{
-  protected:
-    EvalAtomTest():
-      expr_(std::get<0>(GetParam())),
-      expected_(std::get<1>(GetParam()))
-    {}
-
-    std::string expr_;
-    ConsCell *expected_;
-};
-
-using ::testing::Combine;
-using ::testing::Values;
-
-auto Symbols = Values("nil", "t", "'foo", "'foo bar", "'apple pie number 3");
-auto Lists = Values("nil", "'(foo)", "'(foo, bar)", "'(foo . bar)");
-auto Conses = Values("'(foo)", "'(foo, bar)", "'(foo . bar)");
-
-INSTANTIATE_TEST_CASE_P(SymbolsAreAtoms, EvalAtomTest, Combine(Symbols, Values(kT)));
-INSTANTIATE_TEST_CASE_P(ConsessAreNotAtoms, EvalAtomTest, Combine(Conses, Values(kNil)));
-
-TEST_P(EvalAtomTest, Atom)
-{
-  Reader reader("(atom, " + expr_ + ")");
-  ConsCell* exp = reader.Read();
-  EXPECT_EQ(*expected_, *Eval(exp));
-}
-
-TEST(EvalTest, Eq)
-{
-  EXPECT_EQ(*kT, *EvalString("(eq, t, t)"));
-  EXPECT_EQ(*kT, *EvalString("(eq, nil, nil)"));
-  EXPECT_EQ(*kT, *EvalString("(eq, 'foo, 'foo)"));
-
-  EXPECT_EQ(*kNil, *EvalString("(eq, t, nil)"));
-  EXPECT_EQ(*kNil, *EvalString("(eq, 'foo, 'bar)"));
-
-  EXPECT_EQ(*kNil, *EvalString("(eq, 'foo, '(foo))"));
-  EXPECT_EQ(*kNil, *EvalString("(eq, '(foo), '(foo))"));
-}
-
-TEST(EvalTest, Cons)
-{
-  Reader reader("(cons, 'foo, 'bar)");
-  ConsCell *foo = reader::Intern("FOO");
-  ConsCell *bar = reader::Intern("BAR");
-  ConsCell *cons = Cons(foo, bar);
-  EXPECT_EQ(*cons, *Eval(reader.Read()));
-}
-
-TEST(EvalTest, Car)
-{
-  Reader reader("(car, '(foo, bar))");
-  ConsCell *foo = reader::Intern("FOO");
-  EXPECT_EQ(*foo, *Eval(reader.Read()));
-}
-
-TEST(EvalTest, Cdr)
-{
-  Reader reader("(cdr, '(foo, bar))");
-  ConsCell *bar = reader::Intern("BAR");
-  ConsCell *cons = List(bar);
-  EXPECT_EQ(*cons, *Eval(reader.Read()));
-}
-
-TEST(EvalTest, EmptyCondIsNil)
-{
-  Reader reader("(cond)");
-  EXPECT_EQ(*kNil, *Eval(reader.Read()));
-}
-
-TEST(EvalTest, NonMatchingCondIsNil)
-{
-  Reader reader("(cond, (nil, t))");
-  EXPECT_EQ(*kNil, *Eval(reader.Read()));
-}
-
-TEST(EvalTest, NonMatchingCondConsequentIsNotEvaled)
-{
-  Reader reader("(cond, (nil, unbound))");
-  EXPECT_EQ(*kNil, *Eval(reader.Read()));
-}
-
-TEST(EvalTest, MatchingCond)
-{
-  Reader reader("(cond, (nil, t), (t, 'foo))");
-  ConsCell* foo = reader::Intern("FOO");
-  EXPECT_EQ(*foo, *Eval(reader.Read()));
-}
-
-TEST(EvalTest, MatchingCondWithPredicate)
-{
-  Reader reader("(cond, (nil, t), ((atom, 'foo), 'foo))");
-  ConsCell* foo = reader::Intern("FOO");
-  EXPECT_EQ(*foo, *Eval(reader.Read()));
+  EXPECT_EQ(*expect_, *EvalString(expr_));
 }
 
 TEST(EvalTest, MalformedCondThrowsTypeError)
 {
   EXPECT_THROW(EvalString("(cond, t)"), TypeError);
+}
+
+TEST(EvalTest, UnboundSymbol)
+{
+  EXPECT_THROW(EvalString("unbound"), env::UnboundSymbolError);
 }
 } // namespace
